370e9cee847dfc92ab97c090823bf4f65b45c95b
MIV <imivi98@gmail.com>
2023-11-08 19:15:51 +0100

    "Remove some unused imports"

--- src/ejerciciospsp/ejercicio7/Empleado.java
+++ src/ejerciciospsp/ejercicio7/Empleado.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejerciciospsp.ejercicio7;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
