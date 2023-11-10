c3205d5d199c9783c3e0f38889b7423d5a9a56ff
MIV <imivi98@gmail.com>
2023-11-10 21:24:06 +0100

    Start ejercicio8

---
+++ src/ejerciciospsp/ejercicio8/HWWC.java
@@ -0,0 +1,14 @@
+
+package ejerciciospsp.ejercicio8;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MIV
+ */
+public class HWWC {
+    public ArrayList<Meteorito> meteoritos;
+    
+    
+}
---
+++ src/ejerciciospsp/ejercicio8/Main.java
@@ -0,0 +1,12 @@
+
+package ejerciciospsp.ejercicio8;
+
+/**
+ *
+ * @author MIV
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
